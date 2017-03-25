471ba4c194b55c64abeeca5f430c11b6e6c3bda1
Nikos Filippakis <aesmade@gmail.com>
2017-03-25 19:18:26 +0100

    Move respond to new func
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- respond.go
+++ respond.go
@@ -21,6 +21,78 @@ func ip4Checksum(ip4 *layers.IPv4) uint16 {
     }
     return uint16(-sum-1)
 }
+func respondToPacket(packetChannel chan *gopacket.Packet, handle *pcap.Handle) {
+    var eth layers.Ethernet
+    var ip4 layers.IPv4
+    var tcp layers.TCP
+
+    curPacket := *<- packetChannel
+    decoded := []gopacket.LayerType{}
+    parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &tcp)
+    err := parser.DecodeLayers(curPacket.Data(), &decoded)
+
+    if err == nil {
+        var respEth *layers.Ethernet
+        var respIP4 *layers.IPv4
+        var respTCP *layers.TCP
+
+        for _, layerType := range decoded {
+            switch layerType {
+                case layers.LayerTypeEthernet:
+                    respEth = &layers.Ethernet{
+                        DstMAC: eth.SrcMAC,
+                        SrcMAC: eth.DstMAC,
+                        EthernetType: layers.EthernetTypeIPv4,
+                    }
+                case layers.LayerTypeIPv4:
+                    respIP4 = &layers.IPv4{
+                        DstIP: ip4.SrcIP,
+                        SrcIP: ip4.DstIP,
+                        Protocol: layers.IPProtocolTCP,
+                        Version: 4,
+                        IHL: 5,
+                        TTL: 100,
+                    }
+                case layers.LayerTypeTCP:
+                    if tcp.DstPort == 80 {
+                        respTCP = &layers.TCP{
+                            DstPort: tcp.SrcPort,
+                            SrcPort: tcp.DstPort,
+                            Seq: tcp.Ack,
+                            Ack: tcp.Seq + uint32(len(tcp.Payload)),
+                            ACK: true,
+                            Window: 29,
+                            DataOffset: 21,
+                        }
+                    }
+            }
+        }
+        if respEth != nil && respIP4 != nil && respTCP != nil {
+            const payloadStr = "HTTP/1.1 404 Not Found\r\n"
+            buf := gopacket.NewSerializeBuffer()
+            opts := gopacket.SerializeOptions{}
+            payload := gopacket.Payload(payloadStr)
+
+            respIP4.Checksum = ip4Checksum(respIP4)
+            respTCP.SetNetworkLayerForChecksum(respIP4)
+            respTCP.Payload = payload
+            respTCP.Checksum, err = respTCP.ComputeChecksum()
+
+            gopacket.SerializeLayers(buf, opts,
+                respEth,
+                respIP4,
+                respTCP,
+                payload)
+            packetData := buf.Bytes()
+
+            err = handle.WritePacketData(packetData)
+            fmt.Printf("Replying to %s::%d\n", ip4.SrcIP.String(), tcp.SrcPort)
+            if err != nil {
+                fmt.Println(err)
+            }
+        }
+    }
+}
 
 func main() {
     var (
@@ -47,80 +119,7 @@ func main() {
             continue
         }
         lastPacket = packet
-
-        go func(packetChannel chan *gopacket.Packet) {
-            var eth layers.Ethernet
-            var ip4 layers.IPv4
-            var tcp layers.TCP
-
-            curPacket := *<- packetChannel
-            decoded := []gopacket.LayerType{}
-            parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &tcp)
-            err := parser.DecodeLayers(curPacket.Data(), &decoded)
-
-            if err == nil {
-                var respEth *layers.Ethernet
-                var respIP4 *layers.IPv4
-                var respTCP *layers.TCP
-
-                for _, layerType := range decoded {
-                    switch layerType {
-                        case layers.LayerTypeEthernet:
-                            respEth = &layers.Ethernet{
-                                DstMAC: eth.SrcMAC,
-                                SrcMAC: eth.DstMAC,
-                                EthernetType: layers.EthernetTypeIPv4,
-                            }
-                        case layers.LayerTypeIPv4:
-                            respIP4 = &layers.IPv4{
-                                DstIP: ip4.SrcIP,
-                                SrcIP: ip4.DstIP,
-                                Protocol: layers.IPProtocolTCP,
-                                Version: 4,
-                                IHL: 5,
-                                TTL: 100,
-                            }
-                        case layers.LayerTypeTCP:
-                            if tcp.DstPort == 80 {
-                                respTCP = &layers.TCP{
-                                    DstPort: tcp.SrcPort,
-                                    SrcPort: tcp.DstPort,
-                                    Seq: tcp.Ack,
-                                    Ack: tcp.Seq + uint32(len(tcp.Payload)),
-                                    ACK: true,
-                                    Window: 29,
-                                    DataOffset: 21,
-                                }
-                            }
-                    }
-                }
-                if respEth != nil && respIP4 != nil && respTCP != nil {
-                    const payloadStr = "HTTP/1.1 404 Not Found\r\n"
-                    buf := gopacket.NewSerializeBuffer()
-                    opts := gopacket.SerializeOptions{}
-                    payload := gopacket.Payload(payloadStr)
-
-                    respIP4.Checksum = ip4Checksum(respIP4)
-                    respTCP.SetNetworkLayerForChecksum(respIP4)
-                    respTCP.Payload = payload
-                    respTCP.Checksum, err = respTCP.ComputeChecksum()
-
-                    gopacket.SerializeLayers(buf, opts,
-                        respEth,
-                        respIP4,
-                        respTCP,
-                        payload)
-                    packetData := buf.Bytes()
-
-                    err = handle.WritePacketData(packetData)
-                    fmt.Printf("Replying to %s::%d\n", ip4.SrcIP.String(), tcp.SrcPort)
-                    if err != nil {
-                        fmt.Println(err)
-                    }
-                }
-            }
-        }(packetChannel)
-
+        go respondToPacket(packetChannel, handle)
         packetChannel <- &packet
     }
 }
